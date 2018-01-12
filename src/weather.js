4dccffd290df86f6c9df28ff923c99cff545ca4a
Stephen Kane <steve.kane@live.com>
2018-01-11 22:00:10 -0500

    Cache the weather data per zip code. Forever.

--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,6 @@
 /** Reach out to Weather Underground to get the forecast. */
 const nimbus = require('@nnelson/nimbus')();
-const dataFeature = nimbus.getDataFeaturesService('<API_KEY>', '<DATAFEATURE>');
+const weatherCache = {};
 
 function getUrl(key, zip) {
   return 'http://api.wunderground.com/api/' + key + '/forecast/q/' + zip + '.json';
@@ -14,8 +14,12 @@ class WeatherClient {
 
   getWeather(zip, callback) {
     if (!zip) return;
+    if (weatherCache[zip]) return callback(weatherCache[zip]);
     this.dataFeature.getDataByZipCodeUS(zip)
-      .then(callback)
+      .then(function(data) {
+        weatherCache[zip] = data;
+        callback(data);
+      })
       .catch(function(error) {
         console.log('something bad is caught', error);
       });
